d2354eb3e5c6820e1cf9b044105b85fbda9ee46f
Elisafa Borges <Elisafa Borges@ElisafaBorges>
2015-09-05 00:49:51 -0300

    Primeira versão do Projeto final

--- src/gui/GuiCadastroDeAlunos.java
+++ src/gui/GuiCadastroDeAlunos.java
@@ -2,6 +2,8 @@
 package gui;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.ParseException;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -35,7 +37,7 @@ public class GuiCadastroDeAlunos extends JPanel {
         lbBairro = new JLabel("Bairro");
         lbCidade = new JLabel("Cidade");
         lbComplemento = new JLabel("Complemento");
-        lbDataMatricula = new JLabel("Data da Matrícula");
+        lbDataMatricula = new JLabel("Dt. Matrícula");
         lbEmail = new JLabel("E-mail");
         lbMatricula = new JLabel("Matrícula");
         lbNome = new JLabel("Nome");
@@ -66,75 +68,105 @@ public class GuiCadastroDeAlunos extends JPanel {
         ftfTelResi = new JFormattedTextField(ftTelResi);
         ftfTelCel = new JFormattedTextField(ftTelCel);
         
-        lbTitulo.setBounds(45, 5, 400, 20);
+        btCadastrar = new JButton("Cadastrar");
+        btCancelar = new JButton("Cancelar");
+        btLimpar = new JButton("Limpar");
+        
+        lbTitulo.setBounds(45, 15, 400, 20);
         lbTitulo.setFont(new Font("Arial", Font.BOLD, 20));
         add(lbTitulo);
         
-        lbNome.setBounds(25, 30, 100, 25);
+        lbNome.setBounds(25, 45, 100, 25);
         add(lbNome);
-        tfNome.setBounds(25, 50, 350, 25);
+        tfNome.setBounds(25, 65, 350, 25);
         add(tfNome);
         
-        lbSexo.setBounds(400, 30, 100, 25);
+        lbSexo.setBounds(400, 45, 100, 25);
         add(lbSexo);
-        rbMasculino.setBounds(380, 50, 40, 25);
+        rbMasculino.setBounds(380, 65, 40, 25);
         add(rbMasculino);
-        rbFeminino.setBounds(430, 50,50, 25);
+        rbFeminino.setBounds(430, 65,50, 25);
         add(rbFeminino);
         
-        lbRua.setBounds(25, 80, 300, 25);
+        lbRua.setBounds(25, 95, 300, 25);
         add(lbRua);
-        tfRua.setBounds(25, 100, 350, 25);
+        tfRua.setBounds(25, 115, 350, 25);
         add(tfRua);
         
-        lbNumero.setBounds(380, 80, 100, 25);
+        lbNumero.setBounds(380, 95, 100, 25);
         add(lbNumero);
-        tfNumero.setBounds(380, 100, 94, 25);
+        tfNumero.setBounds(380, 115, 94, 25);
         add(tfNumero);
         
-        lbBairro.setBounds(170, 130, 100, 25);
+        lbBairro.setBounds(170, 145, 100, 25);
         add(lbBairro);
-        tfBairro.setBounds(170, 150, 150, 25);
+        tfBairro.setBounds(170, 165, 150, 25);
         add(tfBairro);
         
-        lbComplemento.setBounds(25, 130, 100, 25);
+        lbComplemento.setBounds(25, 145, 100, 25);
         add(lbComplemento);
-        tfComplemento.setBounds(25, 150, 140, 25);
+        tfComplemento.setBounds(25, 165, 140, 25);
         add(tfComplemento);
         
-        lbCidade.setBounds(325, 130, 100, 25);
+        lbCidade.setBounds(325, 145, 100, 25);
         add(lbCidade);
-        tfCidade.setBounds(325, 150, 150, 25);
+        tfCidade.setBounds(325, 165, 150, 25);
         add(tfCidade);
         
-        lbEmail.setBounds(170, 180, 100, 25);
+        lbEmail.setBounds(170, 195, 100, 25);
         add(lbEmail);
-        tfEmail.setBounds(170, 200, 310, 25);
+        tfEmail.setBounds(170, 215, 310, 25);
         add(tfEmail);
         
-        lbUf.setBounds(25, 180, 100, 25);
+        lbUf.setBounds(25, 195, 100, 25);
         add(lbUf);
-        cbUf.setBounds(25, 200,140, 25);
+        cbUf.setBounds(25, 215,140, 25);
         add(cbUf);
         
-       lbTelResi.setBounds(25, 230, 100, 25);
+       lbTelResi.setBounds(25, 245, 100, 25);
        add(lbTelResi);
-       ftfTelResi.setBounds(25, 250, 100, 25);
+       ftfTelResi.setBounds(25, 265, 100, 25);
        add(ftfTelResi);
         
-       lbTelCel.setBounds(140, 230, 100, 25);
+       lbTelCel.setBounds(140, 245, 100, 25);
        add(lbTelCel);
-       ftfTelCel.setBounds(140, 250, 100, 25);
+       ftfTelCel.setBounds(140, 265, 100, 25);
        add(ftfTelCel);
        
-       lbDataMatricula.setBounds(255, 230, 100, 25);
+       lbDataMatricula.setBounds(255, 245, 100, 25);
        add(lbDataMatricula);
-       ftfDataMatricula.setBounds(255, 250, 100, 25);
+       ftfDataMatricula.setBounds(255, 265, 70, 25);
        add(ftfDataMatricula);
+       
+       lbMatricula.setBounds(350, 245, 100, 25);
+       add(lbMatricula);
+       tfMatricula.setBounds(350, 265, 70, 25);
+       add(tfMatricula);
+       
+       btCadastrar.setBounds(25,320, 100,25);
+       add(btCadastrar);
+       
+       btLimpar.setBounds(190, 320, 100, 25);
+       add(btLimpar);
+       
+       btCancelar.setBounds(370, 320, 100, 25);
+       add(btCancelar);
     }
     
     public void definirEventos(){
-        
+         btCancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                System.exit(0);
+            }
+        });
+         btLimpar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+//                tfLogin.setText(null);
+//                pfSenha.setText(null);
+            }
+        });
     }
     
 }
